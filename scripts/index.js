9ad251b127e952e17e872d8085e88bd68b486906
Bogdan Mezentsev <mezentseff.bogdan@yandex.ru>
2022-11-22 15:52:01 +0300

    коммит_после_перерыва

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,32 @@
-let profile = document.querySelector('.profile');
-let info = profile.querySelector('.profile-info');
-let edit = info.querySelector('.profile-info__edit-button');
-let close = document.querySelector('.popup__close-icon');
-let popup = document.querySelector('.popup');
-let like = document.querySelector('.element__like');
-let save = document.querySelector('.edit-form__button');
+const buttonEdit = document.querySelector('.profile-info__edit-button');
+const buttonClose = document.querySelector('#close');
+const popup = document.querySelector('.popup');
+const title = document.querySelector('.profile-info__title');
+const subtitle = document.querySelector('.profile-info__subtitle');
+const form = document.querySelector('.edit-form');
+const nameInput = document.querySelector('#name');
+const jobInput = document.querySelector('#job');
+const buttonSave = document.querySelector('.edit-form__button');
 
-// like.classList.remove('element__like_active');
-
-// like.addEventListener('click', function () {
-//   like.classList.toggle('element__like_active');
-// });
-
-popup.classList.remove('popup_opened');
-
-edit.addEventListener('click', function () {
+function popupOpen () {
   popup.classList.remove('popup');
-  popup.classList.add('popup_opened');
-});
+  popup.classList.add('popup_opened')
+}
 
-close.addEventListener('click', function () {
+buttonEdit.addEventListener('click', popupOpen);
+
+function popupClose () {
   popup.classList.add('popup');
   popup.classList.remove('popup_opened');
-});
+}
 
-save.addEventListener('click', function () {
-  popup.classList.add('popup');
-  popup.classList.remove('popup_opened');
-});
+buttonClose.addEventListener('click', popupClose);
 
-let title = document.querySelector('.profile-info__title');
-let subtitle = document.querySelector('.profile-info__subtitle');
-let form = document.querySelector('.edit-form');
-let nameInput = document.querySelector('.edit-form__title');
-let jobInput = document.querySelector('.edit-form__subtitle');
-
-form.onsubmit = function(evt) {
+function formSubmitHandler (evt) {
   evt.preventDefault();
   title.textContent = nameInput.value;
   subtitle.textContent = jobInput.value;
 };
+form.addEventListener('submit', formSubmitHandler);
+
+buttonSave.addEventListener('click', popupClose);
